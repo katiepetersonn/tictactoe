3766e7c5365299be57c033a09f36fd82d4f2b77a
katiepetersonn <katie.peterson19@gmail.com>
2017-04-12 12:36:13 +1000

    Init

---
+++ README.md
@@ -0,0 +1,44 @@
+Project 0 - Tic Tac Toe
+
+Objective: Build a tic tac toe game in HTML and JavaScript.
+
+Minimum Requirements:
+
+// A user should be able to click on a square.
+// Every click will alternate between marking an X and O.
+// A cell should not be able to be replayed once marked.
+// If a row, column or diagonal is filled with the O, player one will have won the game.
+// If a row, column or diagonal is filled with the X, player two will have won the game.
+// Add a reset button that will clear the contents of the board.
+
+Approach taken:  
+
+HTML:
+
+//Created a index.html. Added necessary HTML tags, including script and link tags to link the JavaScript and CSS respectively, ensured these were connected using console.log and changing the body background.
+// Constructed a "gameboard" using the table tag, so it displayed a 3x3 grid. Used id's so I could target the individual boxes.
+// Added a "reset" button.
+
+JavaSript:
+
+// Started JavaScript next. Located the $".box" elements first and added a event listener. Used the console.log to make sure the selection worked.  
+//After finding the elements, start writing the logic for the click events on those elements and whether or not an X or 0 is to be displayed.
+//Determined the winning combinations and used if/ else statements to log these out.
+//After the necessary moves have been played, check those combinations, and alert the winner if one player ends up winning with three in a row/column or diagonal.
+//Display a message to indicate which turn is about to be played.
+
+CSS:
+
+//Lastly, I styled the game using CSS, the sweet alert library and animate library (added the script tags to the HTML file).
+//Kept the X and O's and used google fonts for the text.
+//Centred the board, changed the colour scheme.
+//Used animated flipInX on the boxes.
+//Used a hover effect on the "reset".
+//Used a sweet alert for the winner. Changed the sweet alert colours to fit in with the colours theme of the game.
+
+
+Unsolved problems:
+
+//Would like to add a winner counter to keep track of multiple game rounds.
+//How to personalise the game, using customised tokens.
+//Support networked multiplayer.
---
+++ js/main.js
@@ -0,0 +1,158 @@
+//SWITCH PLAYERS
+  //   if x then make it 0
+  //   else make it X
+  //  Use a variable to detemine the current player
+
+//CHECK FOR A WINNER
+  //  Use the text in each box
+
+//Event handlers
+  //Use event handlers to change the text
+  //click handlers - who is the current player?
+
+//DISPLAY WINNER
+
+//SEPERATE GAME LOGIC FROM PRESENTATION
+//var playerOne = O;
+//var playerTwo = X;
+
+$(document).ready (function () {
+
+  var player = 1;
+  $(".box").one("click", function () {
+    $(this).addClass("animated flipInX");
+      console.log(this);
+    if(player === 1){
+      $(this).text("O");
+      $("#whoseGo").text("twos ");
+     player = 2;
+    } else {
+     $(this).text("X");
+     $("#whoseGo").text("ones ");
+     player = 1;
+   }
+  });
+
+var playerOneWins = function () {
+  window.setTimeout( function () {
+      swal({
+          title: "Player One Wins",
+          text: "You are a legend!",
+          closeOnConfirm: true,
+          timer: 5000
+      })}, 1000);
+};
+
+var playerTwoWins = function () {
+  window.setTimeout( function () {
+      swal({
+        title: "Player Two Wins",
+        text: "You are a superstar!",
+        closeOnConfirm: true,
+        timer: 5000
+    })}, 1000);
+};
+
+//if all boxes in a row ===  X
+      //then playerOne wins
+    //Else if all boxes equal 0
+      //then playerTwo wins
+
+//first row win
+  $(".box").click(function () {
+    if ($("#box1").text() === "O" && $("#box2").text() === "O" && $("#box3").text() === "O") {
+      playerOneWins();
+    } else if ($("#box1").text() === "X" && $("#box2").text() === "X" && $("#box3").text() === "X") {
+      playerTwoWins();
+  };
+});
+
+//second row win
+
+  $(".box").click(function () {
+    if ($("#box4").text() === "O" && $("#box5").text() === "O" && $("#box6").text() === "O") {
+      playerOneWins();
+  } else if ($("#box4").text() === "X" && $("#box5").text() === "X" && $("#box6").text() === "X") {
+    playerTwoWins();
+  };
+  });
+
+//third row win
+
+  $(".box").click(function () {
+    if ($("#box7").text() === "O" && $("#box8").text() === "O" && $("#box9").text() === "O") {
+      playerOneWins();
+  } else if ($("#box7").text() === "X" && $("#box8").text() === "X" && $("#box9").text() === "X") {
+    playerTwoWins();
+  };
+  });
+
+
+//if all boxes in a column === X
+      //then playerOne wins
+    //Else if all boxes in a column equal 0
+      //then playerTwo wins
+
+//first column win
+
+  $(".box").click(function () {
+    if ($("#box1").text() === "O" && $("#box4").text() === "O" && $("#box7").text() === "O") {
+      playerOneWins();
+  } else if ($("#box1").text() === "X" && $("#box4").text() === "X" && $("#box7").text() === "X") {
+    playerTwoWins();
+  };
+  });
+
+//second column win
+
+  $(".box").click(function () {
+    if ($("#box2").text() === "O" && $("#box5").text() === "O" && $("#box8").text() === "O") {
+      playerOneWins();
+  } else if ($("#box2").text() === "X" && $("#box5").text() === "X" && $("#box8").text() === "X") {
+      playerTwoWins();
+  };
+  });
+
+//third column win
+
+  $(".box").click(function () {
+    if ($("#box3").text() === "O" && $("#box6").text() === "O" && $("#box9").text() === "O") {
+      playerOneWins();
+  }  else if ($("#box3").text() === "X" && $("#box6").text() === "X" && $("#box9").text() === "X") {
+      playerTwoWins();
+  };
+  });
+
+//if all boxes in a diagonal === X
+  //then playerOne wins
+//Else if all boxes in a diagonal equal 0
+  //then playerTwo wins
+
+  //right diagonal win
+
+  $(".box").click(function () {
+    if ($("#box3").text() === "O" && $("#box5").text() === "O" && $("#box7").text() === "O") {
+      playerOneWins();
+  } else if ($("#box3").text() === "X" && $("#box5").text() === "X" && $("#box7").text() === "X") {
+    playerTwoWins();
+  };
+  });
+
+//left diagonal win
+
+  $(".box").click(function () {
+    if ($("#box1").text() === "O" && $("#box5").text() === "O" && $("#box9").text() === "O") {
+      playerOneWins();
+  } else if ($("#box1").text() === "X" && $("#box5").text() === "X" && $("#box9").text() === "X") {
+      playerTwoWins();
+  };
+  });
+
+//reset board
+
+$(".reset").on("click", function ()  {
+    $("td").empty();
+    window.location.reload(true);
+});
+
+ }); //doc.ready end of
