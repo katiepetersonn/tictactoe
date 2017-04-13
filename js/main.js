f2b9bcf35b9bff9cb7df5acde4b9df40331d6720
katiepetersonn <katie.peterson19@gmail.com>
2017-04-13 11:21:46 +1000

    reset working as intended

--- js/main.js
+++ js/main.js
@@ -23,22 +23,26 @@ $(document).ready (function () {
   var playerOneScore= 0;
   var playerTwoScore= 0;
 
-  $(".box").one("click", function () {
-    $(this).addClass("animated flipInX");
-      console.log(this);
-    if(player === 1){
-      $(this).text("O");
-      $("#whoseGo").text("twos ");
-      player = 2;
-      count += 1;
-    } else {
-       $(this).text("X");
-       $("#whoseGo").text("ones ");
-       player = 1;
-       count += 1;
-   }
-  });
 
+ var checkForClick = function () {
+    $(".box").one("click", function () {
+      $(this).addClass("animated flipInX");
+        console.log(this);
+        // debugger
+      if(player === 1){
+        $(this).text("O");
+        $("#whoseGo").text("twos ");
+        player = 2;
+        count += 1;
+      } else {
+         $(this).text("X");
+         $("#whoseGo").text("ones ");
+         player = 1;
+         count += 1;
+     }
+    });
+ };
+ checkForClick();
 
 var playerOneWins = function () {
   window.setTimeout( function () {
@@ -68,105 +72,125 @@ var playerTwoWins = function () {
       //then playerOne wins
     //Else if all boxes equal 0
       //then playerTwo wins
+// checkForClick();
+var checkForWin = function () {
 
-//first row win
-  $(".box").click(function () {
-    if ($("#box1").text() === "O" && $("#box2").text() === "O" && $("#box3").text() === "O") {
-      playerOneWins();
-    } else if ($("#box1").text() === "X" && $("#box2").text() === "X" && $("#box3").text() === "X") {
+  //first row win
+    $(".box").click(function () {
+      if ($("#box1").text() === "O" && $("#box2").text() === "O" && $("#box3").text() === "O") {
+        playerOneWins();
+      } else if ($("#box1").text() === "X" && $("#box2").text() === "X" && $("#box3").text() === "X") {
+        playerTwoWins();
+    }
+  });
+
+  //second row win
+
+    $(".box").click(function () {
+      if ($("#box4").text() === "O" && $("#box5").text() === "O" && $("#box6").text() === "O") {
+        playerOneWins();
+    } else if ($("#box4").text() === "X" && $("#box5").text() === "X" && $("#box6").text() === "X") {
       playerTwoWins();
-  }
-});
+    }
+    });
 
-//second row win
+  //third row win
 
-  $(".box").click(function () {
-    if ($("#box4").text() === "O" && $("#box5").text() === "O" && $("#box6").text() === "O") {
-      playerOneWins();
-  } else if ($("#box4").text() === "X" && $("#box5").text() === "X" && $("#box6").text() === "X") {
-    playerTwoWins();
-  }
-  });
-
-//third row win
-
-  $(".box").click(function () {
-    if ($("#box7").text() === "O" && $("#box8").text() === "O" && $("#box9").text() === "O") {
-      playerOneWins();
-  } else if ($("#box7").text() === "X" && $("#box8").text() === "X" && $("#box9").text() === "X") {
-    playerTwoWins();
-  }
-  });
-
-
-//if all boxes in a column === X
-      //then playerOne wins
-    //Else if all boxes in a column equal 0
-      //then playerTwo wins
-
-//first column win
-
-  $(".box").click(function () {
-    if ($("#box1").text() === "O" && $("#box4").text() === "O" && $("#box7").text() === "O") {
-      playerOneWins();
-  } else if ($("#box1").text() === "X" && $("#box4").text() === "X" && $("#box7").text() === "X") {
-    playerTwoWins();
-  }
-  });
-
-//second column win
-
-  $(".box").click(function () {
-    if ($("#box2").text() === "O" && $("#box5").text() === "O" && $("#box8").text() === "O") {
-      playerOneWins();
-  } else if ($("#box2").text() === "X" && $("#box5").text() === "X" && $("#box8").text() === "X") {
+    $(".box").click(function () {
+      if ($("#box7").text() === "O" && $("#box8").text() === "O" && $("#box9").text() === "O") {
+        playerOneWins();
+    } else if ($("#box7").text() === "X" && $("#box8").text() === "X" && $("#box9").text() === "X") {
       playerTwoWins();
-  }
-  });
+    }
+    });
 
-//third column win
 
-  $(".box").click(function () {
-    if ($("#box3").text() === "O" && $("#box6").text() === "O" && $("#box9").text() === "O") {
-      playerOneWins();
-  }  else if ($("#box3").text() === "X" && $("#box6").text() === "X" && $("#box9").text() === "X") {
+  //if all boxes in a column === X
+        //then playerOne wins
+      //Else if all boxes in a column equal 0
+        //then playerTwo wins
+
+  //first column win
+
+    $(".box").click(function () {
+      if ($("#box1").text() === "O" && $("#box4").text() === "O" && $("#box7").text() === "O") {
+        playerOneWins();
+    } else if ($("#box1").text() === "X" && $("#box4").text() === "X" && $("#box7").text() === "X") {
       playerTwoWins();
-  }
-  });
+    }
+    });
 
-//if all boxes in a diagonal === X
-  //then playerOne wins
-//Else if all boxes in a diagonal equal 0
-  //then playerTwo wins
+  //second column win
 
-  //right diagonal win
+    $(".box").click(function () {
+      if ($("#box2").text() === "O" && $("#box5").text() === "O" && $("#box8").text() === "O") {
+        playerOneWins();
+    } else if ($("#box2").text() === "X" && $("#box5").text() === "X" && $("#box8").text() === "X") {
+        playerTwoWins();
+    }
+    });
 
-  $(".box").click(function () {
-    if ($("#box3").text() === "O" && $("#box5").text() === "O" && $("#box7").text() === "O") {
-      playerOneWins();
-  } else if ($("#box3").text() === "X" && $("#box5").text() === "X" && $("#box7").text() === "X") {
-    playerTwoWins();
-  }
-  });
+  //third column win
 
-//left diagonal win
+    $(".box").click(function () {
+      if ($("#box3").text() === "O" && $("#box6").text() === "O" && $("#box9").text() === "O") {
+        playerOneWins();
+    }  else if ($("#box3").text() === "X" && $("#box6").text() === "X" && $("#box9").text() === "X") {
+        playerTwoWins();
+    }
+    });
 
-  $(".box").click(function () {
-    if ($("#box1").text() === "O" && $("#box5").text() === "O" && $("#box9").text() === "O") {
-      playerOneWins();
-  } else if ($("#box1").text() === "X" && $("#box5").text() === "X" && $("#box9").text() === "X") {
+  //if all boxes in a diagonal === X
+    //then playerOne wins
+  //Else if all boxes in a diagonal equal 0
+    //then playerTwo wins
+
+    //right diagonal win
+
+    $(".box").click(function () {
+      if ($("#box3").text() === "O" && $("#box5").text() === "O" && $("#box7").text() === "O") {
+        playerOneWins();
+    } else if ($("#box3").text() === "X" && $("#box5").text() === "X" && $("#box7").text() === "X") {
       playerTwoWins();
-  } else if (count === 9) {
-    swal("Its a tie!");
-  }
-  });
+    }
+    });
+
+  //left diagonal win
+
+    $(".box").click(function () {
+
+      if ($("#box1").text() === "O" && $("#box5").text() === "O" && $("#box9").text() === "O") {
+        playerOneWins();
+    } else if ($("#box1").text() === "X" && $("#box5").text() === "X" && $("#box9").text() === "X") {
+        playerTwoWins();
+    } else if (count === 9) {
+      swal("Its a tie!");
+    }
+    });
+};
+
+checkForWin();
+
 
 //reset board
 
 $(".reset").on("click", function ()  {
     $("td").empty();
-    
-    window.location.reload(true);
-});
+
+    // We know that the win is based on the text in each box
+      // We need to reset the text
+    // We know that a lot of elements had click event handlers on them, we don't these to run twice
+      // Turn off all of the old event handlers, so each element only has one
+    // Re-add the event handlers
+
+      $(".box").text('');
+      $(".box").removeClass("animated flipInX");
+      $(".box").off();
+      checkForClick();
+      checkForWin();
+      count = 0;
+    });
+
+    //window.location.reload(true);
 
  }); //doc.ready end of
